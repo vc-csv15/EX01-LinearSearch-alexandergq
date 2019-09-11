e99189c2c21c4515641bf5750f306859c2a4463c
vscilab <f.camarillo124@gmail.com>
2019-09-11 14:14:26 -0700

    alex q 9/11 2:14pm

---
+++ src/linearsearch.cc
@@ -0,0 +1,16 @@
+//
+// Created by vscilab on 9/4/2019.
+//
+
+#include <cstdint>
+#include <cstdlib>
+
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+
+                template <class T>
+                int64_t find(const T& value, T array[], size_t size) {
+                    // put your code here
+                    return -1;
+                }
+
+            }}}}
